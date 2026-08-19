1ae98556ffe7e229748ee3f01b8f6b98a6173155
ADOFO-09 <emmanueladofo011@gmail.com>
2026-08-19 23:57:43 +0000

    app sender email setup

--- cop-abirem-cms/app/Mail/TwoFactorCodeMail.php
+++ cop-abirem-cms/app/Mail/TwoFactorCodeMail.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Helpers\SettingHelper;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class TwoFactorCodeMail extends Mailable
+class TwoFactorCodeMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
